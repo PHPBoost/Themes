ff5fddfc00af443be41ffa1036a7871301d77f3f
Arnaud <genet.arnaud@gmail.com>
2017-02-02 22:00:16 +0100

    [FIX] multitheme

--- Multi_Theme/js/flaunt.js
+++ Multi_Theme/js/flaunt.js
@@ -38,4 +38,31 @@
 	    
 	});
 	
+	// DOM ready
+	$(function() {
+		
+		// Append the mobile icon nav
+		$('.MNnav').append($('<div class="MNnav-mobile"></div>'));
+		
+		// Add a <span> to every .nav-item that has a <ul> inside
+		$('.MNnav-item').has('ul').prepend('<span class="MNnav-click"><i class="MNnav-arrow"></i></span>');
+		
+		// Click to reveal the nav
+		$('.MNnav-mobile').click(function(){
+			$('.MNnav-list').toggle();
+		});
+	
+		// Dynamic binding to on 'click'
+		$('.MNnav-list').on('click', '.MNnav-click', function(){
+		
+			// Toggle the nested nav
+			$(this).siblings('.MNnav-submenu').toggle();
+			
+			// Toggle the arrow using CSS3 transforms
+			$(this).children('.MNnav-arrow').toggleClass('MNnav-rotate');
+			
+		});
+	    
+	});
+	
 })(jQuery);
