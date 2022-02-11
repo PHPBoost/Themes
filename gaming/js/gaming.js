e7bb8b2a04a93930e6db2ef528e380b7bdcceb8e
babsolune <babsolune@phpboost.com>
2022-02-11 04:46:53 +0100

    [UPD] gaming carousel

--- gaming/js/gaming.js
+++ gaming/js/gaming.js
@@ -1,3 +1,4 @@
+// Sticky header
 jQuery(document).ready( () => {
     jQuery(window).scroll( () => {
       let rupture = jQuery('#header').outerHeight() ;
@@ -8,6 +9,8 @@ jQuery(document).ready( () => {
       }
     });
 });
+
+// change header menu icon
 jQuery('#menus-trigger').on('click', () => {
     jQuery('#header-container').toggleClass('active-menus');
     jQuery('#menus-trigger').toggleClass('fa-times error');
@@ -16,3 +19,16 @@ jQuery('#menus-trigger').on('click', () => {
 // top for sicky menu on scroll
 let headerHeight = jQuery('#site-name').outerHeight();
 jQuery('.sticky-menu').css('top', headerHeight + 'px');
+
+// Slider
+jQuery('#gaming-carousel')
+	.addClass('owl-carousel')
+	.owlCarousel({
+		autoplay: true,
+		autoplayTimeout: 7000,
+		loop: true,
+		margin: 15,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		items: 1
+});
