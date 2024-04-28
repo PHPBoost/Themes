29cf3a8bd66545e8517adebe0b46a21f90bfae77
babsolune <babsolune@phpboost.com>
2024-04-28 06:07:53 +0200

    [NEW] FreshUp

---
+++ FreshUp/lang/english/theme.php
@@ -0,0 +1,4 @@
+<?php
+$lang['theme.toggle.menu.left'] = 'Hide/reveal left menus';
+$lang['theme.toggle.menu.right'] = 'Hide/reveal right menus';
+?>
---
+++ FreshUp/lang/french/theme.php
@@ -0,0 +1,4 @@
+<?php
+$lang['theme.toggle.menu.left'] = 'Cacher/montrer les menus de gauche';
+$lang['theme.toggle.menu.right'] = 'Cacher/montrer les menus de droite';
+?>
